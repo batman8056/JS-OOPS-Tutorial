3c090a5dec0f17696e3a87744e96b146e0d288e8
batman8056 <manimanish8056@gmail.com>
2024-12-11 10:56:37 +0530

    notes update

--- 2_Chapter/2_property.js
+++ 2_Chapter/2_property.js
@@ -4,11 +4,18 @@ let person = {name : "manikandan"};
 for(let key in person){
     console.log(key);
 }
+
 //lets print only values within array
 console.log(Object.keys(person));
 
-//
+//get the object PrototypeOf person
 let objectBase = Object.getPrototypeOf(person);
-
 console.log(objectBase);
+
+// it is return object(PropertyDescriptor) to attached toString
+let descriptor=  Object.getOwnPropertyDescriptor(objectBase, 'toString');
+console.log(descriptor);
+
+//
+
 
